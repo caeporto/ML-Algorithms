2cf1f3d74466b83ba3f3c6dcfddf99eb9fd9c3cd
Caetano Porto da Silva <caetanop@dbserver.com.br>
2021-10-12 01:08:35 -0300

    Modifying linear regression dependencies

--- Golang/LinearRegression/go.mod
+++ Golang/LinearRegression/go.mod
@@ -2,18 +2,24 @@ module LinearRegression
 
 go 1.17
 
+replace github.com/caeporto/ML-Algorithms/Golang/GradientDescent v0.0.0 => ../GradientDescent
+
+replace github.com/caeporto/ML-Algorithms/Golang/Util v0.0.0 => ../Util
+
 require (
+	github.com/caeporto/ML-Algorithms/Golang/GradientDescent v0.0.0
+	github.com/caeporto/ML-Algorithms/Golang/Util v0.0.0
 	gonum.org/v1/gonum v0.9.3
-	gonum.org/v1/plot v0.9.0
 )
 
 require (
-	github.com/ajstarks/svgo v0.0.0-20180226025133-644b8db467af // indirect
+	github.com/ajstarks/svgo v0.0.0-20210923152817-c3b6e2f0c527 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
-	github.com/go-fonts/liberation v0.1.1 // indirect
-	github.com/go-latex/latex v0.0.0-20210118124228-b3d85cf34e07 // indirect
+	github.com/go-fonts/liberation v0.2.0 // indirect
+	github.com/go-latex/latex v0.0.0-20210823091927-c0d11ff05a81 // indirect
+	github.com/go-pdf/fpdf v0.5.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/phpdave11/gofpdf v1.4.2 // indirect
-	golang.org/x/image v0.0.0-20210216034530-4410531fe030 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gonum.org/v1/plot v0.10.0 // indirect
 )
--- Golang/LinearRegression/main.go
+++ Golang/LinearRegression/main.go
@@ -1,27 +1,20 @@
 package main
 
 import (
-	"LinearRegression/linearRegression"
+	"github.com/caeporto/ML-Algorithms/Golang/GradientDescent"
+	"github.com/caeporto/ML-Algorithms/Golang/Util"
 	"gonum.org/v1/gonum/mat"
 	"strconv"
 )
 
-func makeRange(min, max int) []float64 {
-	a := make([]float64, max-min+1)
-	for i := range a {
-		a[i] = float64(min + i)
-	}
-	return a
-}
-
 func main(){
-	resources := linearRegression.ReadCSV("./resources/data.csv")
+	resources := Util.ReadCSV("./resources/data.csv")
 
 	m := len(resources)
 	x := mat.NewDense(2, m, nil) //adding another row to X, so we can model the intercept term
 	y := mat.NewDense(1, m, nil)
 
-	for i, _ := range resources {
+	for i := range resources {
 		x.Set(0, i, 1)
 		valx, _ := strconv.ParseFloat(resources[i][0], 64)
 		x.Set(1, i, valx)
@@ -31,23 +24,23 @@ func main(){
 
 	X := x.Slice(1, 2, 0, m).(*mat.Dense) //get just the X-Axis without the additional row
 
-	linearRegression.PlotAndSaveData("Food Truck", "Population (in 10.000s)", "Profit (in $10.000s)", X, y, 1,nil, m, "./resources/points")
+	Util.PlotAndSaveData("Food Truck", "Population (in 10.000s)", "Profit (in $10.000s)", X, y, 1,nil, m, "./resources/points")
 
 	theta := mat.NewDense(2, 1, nil) //theta_0 and theta_1 terms
 
 	iterations := 1500
 	alpha := 0.01
 
-	costHistory := linearRegression.GradientDescent(x, y, theta, iterations, alpha, m)
-	iter := makeRange(1, iterations)
+	costHistory := GradientDescent.GradientDescent(x, y, theta, iterations, alpha, m)
+	iter := Util.MakeRange(1, iterations)
 	xIterations := mat.NewDense(1, iterations, iter)
 	yCost := mat.NewDense(1, iterations, costHistory)
 
 	var ypred mat.Dense
 	ypred.Mul(theta.T(), x)
 
-	linearRegression.PlotAndSaveData("Food Truck", "Population (in 10.000s)", "Profit (in $10.000s)", X, y, 1, &ypred, m, "./resources/predicted")
+	Util.PlotAndSaveData("Food Truck", "Population (in 10.000s)", "Profit (in $10.000s)", X, y, 1, &ypred, m, "./resources/predicted")
 
-	linearRegression.PlotAndSaveData("Cost History", "Iterations", "Cost", xIterations, yCost, 10, nil, iterations, "./resources/cost")
+	Util.PlotAndSaveData("Cost History", "Iterations", "Cost", xIterations, yCost, 10, nil, iterations, "./resources/cost")
 
 }
